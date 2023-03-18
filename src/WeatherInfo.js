c3a38e38ed34ac14116ccb91b037e3a0dadf54a9
Flall <119894671+Flall@users.noreply.github.com>
2023-03-18 21:04:00 +0100

    New Component for C to F conversion

---
+++ src/WeatherInfo.js
@@ -0,0 +1,38 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1 className="mb-2">{props.data.city}</h1>
+      <ul>
+        <li className="text-capitalize">
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="d-flex">
+            <img
+              src={props.data.iconUrl}
+              alt={props.data.description}
+              className="float-left"
+            />
+            <div className="float-left">
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Feels Like: {Math.round(props.data.feelsLike)}°C</li>
+            <li>Humidity: {Math.round(props.data.humidity)}%</li>
+            <li>Wind: {Math.round(props.data.wind)}km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
