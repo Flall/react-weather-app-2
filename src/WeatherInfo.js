3a7e0d3795c2e1e74368ce415c28277c54bdee72
Flall <119894671+Flall@users.noreply.github.com>
2023-03-19 12:10:38 +0100

    Trying to remove image from mobile devices - bugged
    needs to be fixed

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -18,7 +18,7 @@ export default function WeatherInfo(props) {
             <img
               src={props.data.iconUrl}
               alt={props.data.description}
-              className="float-left"
+              className="float-left d-sm-none"
             />
             <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
