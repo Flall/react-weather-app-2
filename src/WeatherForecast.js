5c3feaa5ffad406d5c3bba8597949376534cda99
Flall <119894671+Flall@users.noreply.github.com>
2023-03-19 09:43:44 +0100

    added "else" statement in 6-day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -23,6 +23,8 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
